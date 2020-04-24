7500eefa327f6eb2f861017cbecb9f9cbb62def3
danish-wani <danish.wani@trialx.com>
2020-04-24 16:02:43 +0530

    -Xpaths updated and user interaction improved

--- CreateGitRepo.py
+++ CreateGitRepo.py
@@ -10,14 +10,16 @@ import sys
 import random
 import os
 
-PROJECT_NAME = 'task_demo'
+REPOSITORY_NAME = 'automation_repo'
+
 
 class Project:
-        """
-            Creates a Repository on Github, Clones that to local machine and creates a virtual environment for the same
-        """
-	def __init__(self, project_name):
-		self.project_name = project_name
+	"""
+		Creates a Repository on Github, Clones that to local machine and creates a virtual environment for the same
+	"""
+	def __init__(self, repo_name=REPOSITORY_NAME, private=True):
+		self.repo_name = repo_name
+		self.private = private
 
 	def login(self, username, password):
 		self.username = username
@@ -35,59 +37,61 @@ class Project:
 
 	def create_github_repo(self):
 		options_dropdown = WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable
-			((By.XPATH, "/html/body/div[1]/header/div[7]/details")))
+			((By.XPATH, "/html/body/div[1]/header/div[6]/details")))
 		options_dropdown.click()	#navigates to new repo page
 		new_repo = WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable
-			((By.XPATH, "/html/body/div[1]/header/div[7]/details/details-menu/a[1]")))
+			((By.XPATH, "/html/body/div[1]/header/div[6]/details/details-menu/a[1]")))
 		new_repo.click()
 		repo_name_field = WebDriverWait(self.bot, 10).until(EC.presence_of_element_located((By.ID, "repository_name")))
 		repo_name_field.clear()
-		repo_name_field.send_keys(self.project_name)
+		repo_name_field.send_keys(self.repo_name)
 
 		# if provided repo name already exists
+		sleep(2)
+		new_name = str()
 		while 'is-autocheck-successful' not in repo_name_field.get_attribute('class'):
 			sleep(2)
 			repo_name_field.clear()
-			new_name = self.project_name+str(random.randint(0,9))
+			new_name = self.repo_name+str(random.randint(0,9))
 			repo_name_field.send_keys(new_name)
 			sleep(2)
 
-		# uncomment below code if you want to take input from the user for the visibility of Repo
-		# self.visibility = str(input('Do you want to make this Repostory Private? Y/N          '))
-		# if self.visibility.lower() == 'y':
-			# WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_visibility_private"]'))).click()
-		# else:
-			# WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_visibility_public"]'))).click()
+		if self.private:
+			WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_visibility_private"]'))).click()
+		else:
+			WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_visibility_public"]'))).click()
+		
 		WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_visibility_public"]'))).click()	#make the repo public
 		WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="repository_auto_init"]'))).click()	#initialize th readme
 		WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="new_repository"]/div[3]/button'))).click()	#submit and create the repo
-		print('**Repository on Github successfully created by the name {0}**'.format(new_name))
+		print('**Repository on Github successfully created by the name {0}**'.format(new_name if new_name else self.repo_name))
 
 	def get_github_repo_click(self):
 		WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable
-			((By.XPATH,'/html/body/div[4]/div/main/div[2]/div[1]/div[3]/details[2]/summary'))).click()
+			((By.XPATH,'/html/body/div[4]/div/main/div[2]/div/div[3]/span/get-repo-controller/details/summary'))).click()
 		link_field = WebDriverWait(self.bot, 10).until(EC.element_to_be_clickable
-			((By.XPATH,'/html/body/div[4]/div/main/div[2]/div[1]/div[3]/details[2]/div/div/div[1]/div[1]/div/input')))
+			((By.XPATH,'/html/body/div[4]/div/main/div[2]/div/div[3]/span/get-repo-controller/details/div/div/div[1]/div[1]/div/input')))
 		self.repo_link = link_field.get_attribute("value")
 
 	def clone_repo_locally(self):
-		os.chdir("/home/danish/Workspace/projects/")
 		os.system("git clone {0}".format(self.repo_link))
 		print('**Git Repo cloned successfully**')
 
 	def create_virtualenv(self):
-		os.chdir("/home/danish/Workspace/envs/")
-		os.system("sudo virtualenv -p python3 env_{0}".format(self.project_name))
-		print('**Virtualenv by the name env_{0} created successfully**'.format(self.project_name))
+		os.system("sudo virtualenv -p python3 virtualenv_{0}".format(self.repo_name))
+		print('**Virtualenv by the name env_{0} created successfully**'.format(self.repo_name))
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		project_name = str(sys.argv[1])
-	else:
-		project_name = PROJECT_NAME
-	project_task = Project(project_name)
-	project_task.login('username','password')
+	username, password, repo_name = [value.strip() for value in str(input('Enter username, password and repository name, separted by comma(,): \n')).split(',')]
+	print(username)
+	print(password)
+	private = str(input('Do you want to keep the repository private? (Y/N): \n'))
+	private = True if private.lower() == 'y' else False
+	if not repo_name:
+		repo_name = REPOSITORY_NAME
+	project_task = Project(repo_name=repo_name, private=private)
+	project_task.login(username, password)
 	project_task.create_github_repo()
 	project_task.get_github_repo_click()
 	sleep(3)
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 1. An Instagram Bot that searches for a provided hashtag and likes few posts containing that hashtag.
 2. Another python bot that creates git hub repo, clones that locally and creates a virtual environment for the same.
 
-Download python3 and pip3
-Download geckodriver and copy that to /usr/local/bin/
-run pip install -r requirements.txt
+*Download python3 and pip3
+*Download geckodriver and copy that to /usr/local/bin/  https://askubuntu.com/questions/870530/how-to-install-geckodriver-in-ubuntu
+*run pip install -r requirements.txt
